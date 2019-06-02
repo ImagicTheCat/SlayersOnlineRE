05915ab834de730984fe6fdcfd332a574f17764d
imagic <>
2019-06-02 07:36:58 +0000

    Add basic entity system.
    FossilOrigin-Name: 481a55776d1536894bbf08be894ee74769dfb32f6671de0f5449b4e24e3943f6

--- src/server/Client.lua
+++ src/server/Client.lua
@@ -1,25 +1,43 @@
 local msgpack = require("MessagePack")
 local net = require("protocol")
+local Entity = require("Entity")
 
 -- server-side client
-local Client = class("Client")
+local Client = class("Client", Entity)
+
+-- STATICS
+
+function Client.makePacket(protocol, data)
+  return msgpack.pack({protocol, data})
+end
+
+-- METHODS
 
 function Client:__construct(server, peer)
+  Entity.__construct(self)
+
   self.server = server
   self.peer = peer
 
-  self:sendPacket(net.PROTOCOL, net) -- send protocol
+  self:send(Client.makePacket(net.PROTOCOL, net)) -- send protocol
 end
 
 function Client:onPacket(protocol, data)
 end
 
 -- unsequenced: unsequenced and unreliable if true/passed, reliable otherwise
-function Client:sendPacket(protocol, data, unsequenced)
-  self.peer:send(msgpack.pack({protocol, data}), 0, (unsequenced and "unsequenced" or "reliable"))
+function Client:send(packet, unsequenced)
+  self.peer:send(packet, 0, (unsequenced and "unsequenced" or "reliable"))
 end
 
 function Client:onDisconnect()
+end
+
+-- overload
+function Client:onMapChange()
+  if self.map then
+    self:send(Client.makePacket(net.MAP), self.map:serializeNet())
+  end
 end
 
 return Client
