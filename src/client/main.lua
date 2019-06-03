8e91dd10b403ec5554254b9a767675ddc738275f
Imagic <imagicthecat@gmail.com>
2019-06-03 19:22:55 +0200

    Basic movement controls.

--- src/client/main.lua
+++ src/client/main.lua
@@ -18,3 +18,10 @@ end
 function love.draw()
   client:draw()
 end
+
+function love.keypressed(key, scancode)
+  if scancode == "w" then client:setOrientation(0)
+  elseif scancode == "d" then client:setOrientation(1)
+  elseif scancode == "s" then client:setOrientation(2)
+  elseif scancode == "a" then client:setOrientation(3) end
+end
