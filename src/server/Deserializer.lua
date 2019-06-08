2746c366667c25db991413a02b51b7542c1148c8
imagic <>
2019-06-08 14:57:46 +0000

    Add map events (.ev0) parsing and doc fix.
    FossilOrigin-Name: 9769e7212bee1b9975609f2973f5953fffca392b395aaf96c7adc6a9a0681b1c

--- src/server/Deserializer.lua
+++ src/server/Deserializer.lua
@@ -114,6 +114,7 @@ function Deserializer.loadMapEvents(id)
 
   if f_evn and f_ev0 then
     local events = {}
+    local events_per_coords = {}
 
     -- evn
     local count = f_evn:seek("end")/344 -- 344 bytes per event entry
@@ -122,6 +123,40 @@ function Deserializer.loadMapEvents(id)
     for i=1,count do
       local event = Deserializer.readMapEventEntry(f_evn)
       table.insert(events, event)
+
+      -- reference per coords
+      local key = event.x..","..event.y
+      local coords_events = events_per_coords[key]
+      if not coords_events then
+        coords_events = {}
+        events_per_coords[key] = coords_events
+      end
+
+      event.conditions = {}
+      event.commands = {}
+      table.insert(coords_events, event)
+    end
+
+    -- ev0
+    local line = f_ev0:read("*l")
+    while line do
+      local ltype,x,y,page,index,instruction = string.match(line, "(..)(%d+),(%d+),(%d+),(%d+)=(.*)")
+
+      if ltype then -- match
+        local coords_events = events_per_coords[x..","..y] -- get events by coords
+        if coords_events then
+          local event = coords_events[tonumber(index)+1] -- get event by page
+          if event then
+            if ltype == "EV" then -- event commands
+              table.insert(event.commands, instruction)
+            elseif ltype == "CD" then -- event conditions
+              table.insert(event.conditions, instruction)
+            end
+          end
+        end
+      end
+
+      line = f_ev0:read("*l")
     end
 
     return events
