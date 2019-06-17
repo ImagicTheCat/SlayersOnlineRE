150cd2d399f67a5a01b6167fb1a8b83107694647
Imagic <imagicthecat@gmail.com>
2019-06-17 17:04:08 +0200

    Add event visual effect animations.

--- src/client/entities/Event.lua
+++ src/client/entities/Event.lua
@@ -24,9 +24,16 @@ Event.Animation = {
 function Event:__construct(data)
   Entity.__construct(self, data)
 
+  self.animation_type = data.animation_type
+
   self.anim_x = data.animation_number or 0
   self.anim_y = data.orientation or 0
-  self.animation_type = data.animation_type
+
+  self.anim_wc = data.animation_wc or 0
+  self.anim_hc = data.animation_hc or 0
+  self.anim_index = 1
+  self.anim_interval = 1/3
+  self.anim_time = 0
 
   self.set = client:loadTexture("resources/textures/sets/"..data.set)
   self.atlas = TextureAtlas(data.set_x, data.set_y, self.set:getWidth(), self.set:getHeight(), data.w, data.h)
@@ -52,9 +59,13 @@ function Event:onPacket(action, data)
   elseif action == "ch_active" then
     self.active = data
   elseif action == "ch_animation_type" then
+    self.animation_type = data.animation_type
+
     self.anim_x = data.animation_number or 0
     self.anim_y = data.orientation or 0
-    self.animation_type = data.animation_type
+
+    self.anim_wc = data.animation_wc or 0
+    self.anim_hc = data.animation_hc or 0
   elseif action == "ch_set_dim" then
     self.atlas = TextureAtlas(data.x, data.y, self.set:getWidth(), self.set:getHeight(), data.w, data.h)
   elseif action == "ch_animation_number" then
@@ -64,6 +75,16 @@ end
 
 -- overload
 function Event:tick(dt)
+  if self.active and self.animation_type == Event.Animation.VISUAL_EFFECT then
+    self.anim_time = self.anim_time+dt
+
+    local steps = math.floor(self.anim_time/self.anim_interval)
+    self.anim_time = self.anim_time-steps*self.anim_interval
+
+    self.anim_index = (self.anim_index+steps)%(self.anim_wc*self.anim_hc)
+    self.anim_x = self.anim_index%self.anim_wc
+    self.anim_y = math.floor(self.anim_index/self.anim_wc)
+  end
 end
 
 -- overload
