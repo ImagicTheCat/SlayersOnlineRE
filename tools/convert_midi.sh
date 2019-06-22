7632d04ea8e39e3386e5cc2b408be016f3411e7b
Imagic <imagicthecat@gmail.com>
2019-06-22 17:53:48 +0200

    Add loudnorm filter to midi converter.

--- tools/convert_midi.sh
+++ tools/convert_midi.sh
@@ -10,4 +10,4 @@ fi
 
 path=$1
 
-find . -type f -name '*.mid' | sed -e 's/^\(.\+\)\.mid$/\1/g' | xargs -d '\n' -P $(nproc) -n 1 -I % bash -c "fluidsynth -F \"%.wav\" \"$path\" \"%.mid\" 2> /dev/null && ffmpeg -y -i \"%.wav\" -vn -c:a libvorbis -b:a 128k \"%.ogg\" && rm \"%.wav\""
+find . -type f -name '*.mid' | sed -e 's/^\(.\+\)\.mid$/\1/g' | xargs -d '\n' -P $(nproc) -n 1 -I % bash -c "fluidsynth -F \"%.wav\" \"$path\" \"%.mid\" 2> /dev/null && ffmpeg -y -i \"%.wav\" -vn -c:a libvorbis -b:a 128k -filter:a loudnorm -ar 44100 \"%.ogg\" && rm \"%.wav\""
