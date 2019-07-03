cd189d9d1b4221ca640bc699cc935c8946cf199a
imagic <>
2019-07-03 08:27:58 +0000

    Generalize some Window properties to Widget class.
    FossilOrigin-Name: 0e3d1ce5af8e1eadc1e0c1401188d3b05c095d1b1ed168292872f79122ede28c

--- src/client/gui/Window.lua
+++ src/client/gui/Window.lua
@@ -1,64 +1,13 @@
+local Widget = require("gui/Widget")
 
-local Window = class("Window")
-
--- STATICS
-
-local system
-
-function Window.loadSystemBorders(x, y, w, h, margin)
-  local borders = {
-    x = x,
-    y = y,
-    w = w,
-    h = h,
-    margin = margin
-  }
-
-  -- corners
-  -- top
-  borders.ctl = love.graphics.newQuad(x,y,margin,margin,160,80)
-  borders.ctr = love.graphics.newQuad(x+w-margin,y,margin,margin,160,80)
-  -- bottom
-  borders.cbl = love.graphics.newQuad(x,y+h-margin,margin,margin,160,80)
-  borders.cbr = love.graphics.newQuad(x+w-margin,y+h-margin,margin,margin,160,80)
-
-  -- middle
-  borders.mt = love.graphics.newQuad(x+margin,y,y+h-margin*2,margin,160,80)
-  borders.mb = love.graphics.newQuad(x+margin,y+h-margin,w-margin*2,margin,160,80)
-  borders.ml = love.graphics.newQuad(x,margin,margin,h-margin*2,160,80)
-  borders.mr = love.graphics.newQuad(x+w-margin,margin,margin,h-margin*2,160,80)
-
-  return borders
-end
-
-function Window.loadSystem(client)
-  if not system then
-    system = {}
-
-    system.tex = client:loadTexture("resources/textures/system.png")
-    system.background = love.graphics.newQuad(0,0,32,32,160,80)
-
-    system.window_borders = Window.loadSystemBorders(32, 0, 32, 32, 5)
-    system.select_borders = Window.loadSystemBorders(64, 0, 32, 32, 5)
-  end
-
-  return system
-end
+local Window = class("Window", Widget)
 
 -- METHODS
 
 function Window:__construct(client)
-  self.client = client
+  Widget.__construct(self, client)
 
-  self.x = 0
-  self.y = 0
-  self.w = 0
-  self.h = 0
   self.system = Window.loadSystem(client)
-end
-
-function Window:update(x, y, w, h)
-  self.x, self.y, self.w, self.h = x,y,w,h
 end
 
 -- draw rect based on borders
@@ -85,6 +34,11 @@ function Window:draw()
   love.graphics.draw(self.system.tex, self.system.background, x+1, y+1, 0, (w-2)/32, (h-2)/32)
 
   self:drawBorders(self.system.window_borders, x, y, w, h)
+end
+
+function Window:clip()
+  local scale = self.client.gui_scale
+  love.graphics.setScissor((self.x+3)*scale, (self.y+3)*scale, (self.w-6)*scale, (self.h-6)*scale)
 end
 
 return Window
