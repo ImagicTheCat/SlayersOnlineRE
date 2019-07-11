33375a8c4f8fdea25e66b16045c432cf8a9b902f
Imagic <imagicthecat@gmail.com>
2019-07-11 15:04:50 +0200

    Add basic database tables, fix manager issue.

---
+++ tools/db/create.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users(
+  id INTEGER UNSIGNED AUTO_INCREMENT,
+  pseudo VARCHAR(50) UNIQUE,
+  password BINARY(64),
+  CONSTRAINT pk_users PRIMARY KEY(id)
+);
+
+CREATE TABLE users_int_vars(
+  id INTEGER UNSIGNED,
+  user_id INTEGER UNSIGNED,
+  value INTEGER,
+  CONSTRAINT pk_users_int_vars PRIMARY KEY(id),
+  CONSTRAINT fk_users_int_vars_users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE users_bool_vars(
+  id INTEGER UNSIGNED,
+  user_id INTEGER UNSIGNED,
+  value TINYINT,
+  CONSTRAINT pk_users_bool_vars PRIMARY KEY(id),
+  CONSTRAINT fk_users_bool_vars_users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE server_vars(
+  id VARCHAR(200),
+  value TEXT,
+  CONSTRAINT pk_server_vars PRIMARY KEY(id)
+);
---
+++ tools/db/delete.sql
@@ -0,0 +1,4 @@
+DROP TABLE users_bool_vars;
+DROP TABLE users_int_vars;
+DROP TABLE users;
+DROP TABLE server_vars;
