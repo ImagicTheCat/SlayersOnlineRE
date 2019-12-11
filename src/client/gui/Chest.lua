bf491c7dbb270a46f748259f8cbc60343b30fa91
imagic <>
2019-12-11 19:03:41 +0000

    Begin chest GUI.
    FossilOrigin-Name: a938bbb13aca19ef422b42347ffd212985b3ae0b7ce8aefea690bb5f69fc1c0c

---
+++ src/client/gui/Chest.lua
@@ -0,0 +1,84 @@
+local Widget = require("ALGUI.Widget")
+local Window = require("gui.Window")
+local GridInterface = require("gui.GridInterface")
+local Text = require("gui.Text")
+local TextInput = require("gui.TextInput")
+
+local Chest = class("Chest", Widget)
+
+-- PRIVATE STATICS
+
+local COLUMNS = 2
+
+-- METHODS
+
+function Chest:__construct()
+  Widget.__construct(self)
+
+  -- title
+  self.w_title = Window("vertical")
+  self.title = Text("title")
+  self.w_title.content:add(self.title)
+  self:add(self.w_title)
+
+  -- player gold
+  self.w_gold_l = Window("vertical")
+  self.gold_l = GridInterface(2,2,"vertical")
+  self.gold_l:set(0,0,Text("Gold:"))
+  self.gold_l:set(0,1,Text("Trade:"))
+  self.gold_l:set(1,0,Text("0"))
+  self.gold_l:set(1,1,TextInput())
+  self.w_gold_l.content:add(self.gold_l)
+  self:add(self.w_gold_l)
+
+  -- chest gold
+  self.w_gold_r = Window("vertical")
+  self.gold_r = GridInterface(2,2,"vertical")
+  self.gold_r:set(0,0,Text("Gold:"))
+  self.gold_r:set(0,1,Text("Trade:"))
+  self.gold_r:set(1,0,Text("0"))
+  self.gold_r:set(1,1,TextInput())
+  self.w_gold_r.content:add(self.gold_r)
+  self:add(self.w_gold_r)
+
+  -- inventory content
+  self.w_content_l = Window()
+  self.content_l = GridInterface(0,0)
+  self.w_content_l.content:add(self.content_l)
+  self:add(self.w_content_l)
+
+  -- chest content
+  self.w_content_r = Window()
+  self.content_r = GridInterface(0,0)
+  self.w_content_r.content:add(self.content_r)
+  self:add(self.w_content_r)
+
+  -- info
+  self.w_info = Window("vertical")
+  self.info = Text("info")
+  self.w_info.content:add(self.info)
+  self:add(self.w_info)
+end
+
+-- override
+function Chest:updateLayout(w,h)
+  self:setSize(w,h)
+
+  self.w_title:updateLayout(w,h)
+
+  self.w_gold_l:setPosition(0, self.w_title.h)
+  self.w_gold_l:updateLayout(w/2,h)
+  self.w_gold_r:setPosition(w/2, self.w_title.h)
+  self.w_gold_r:updateLayout(w/2,h)
+
+  self.w_info:updateLayout(w,h)
+  self.w_info:setPosition(0,h-self.w_info.h)
+
+  local content_y = self.w_gold_l.y+math.max(self.w_gold_l.h, self.w_gold_r.h)
+  self.w_content_l:setPosition(0, content_y)
+  self.w_content_l:updateLayout(w/2, h-content_y-self.w_info.h)
+  self.w_content_r:setPosition(w/2, content_y)
+  self.w_content_r:updateLayout(w/2, h-content_y-self.w_info.h)
+end
+
+return Chest
