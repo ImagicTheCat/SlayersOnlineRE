c6b1d1b69026029f1e7d1c4a1c3204db6f0a03fc
imagic <>
2020-05-05 20:25:09 +0000

    Add database reconnection.
    FossilOrigin-Name: 8c58200a15841ae9731033b39faf3d841125fdb3f6314c17d4d6a3d8504155f5

--- src/server/DBManager.lua
+++ src/server/DBManager.lua
@@ -7,57 +7,78 @@ local DBManager = class("DBManager")
 
 -- thread
 local function thread(ch_in, ch_out, db, user, password, host, port)
+  local CONNECT_DELAY = 5 -- seconds
   local mysql_driver = require("luasql.mysql")
   local mysql_env = mysql_driver.mysql()
   local msgpack = require("MessagePack")
+  local ffi = require("ffi")
+  ffi.cdef("unsigned int sleep(unsigned int nb_sec);")
+  local C = ffi.C
+  local con
 
-  local con = mysql_env:connect(db, user, password, host, port)
-  if con then
-    print("connected to database")
-  else
-    print("couldn't connect to database")
-    return
+  -- connect to DB (loop)
+  local function connect()
+    local connecting = true
+    while connecting do
+      con = mysql_env:connect(db, user, password, host, port)
+      if con then
+        print("connected to database")
+        connecting = false
+      else
+        print("connection to database failed, waiting "..CONNECT_DELAY.."s")
+        C.sleep(CONNECT_DELAY)
+      end
+    end
   end
 
+  -- return affected, last insert id
+  -- or rows
+  -- or nil, error
+  local function process_query(query)
+    local r, err = con:execute(query)
+    if r == nil then -- error
+      print(err, "["..query.."]")
+      return r,err
+    elseif type(r) ~= "userdata" then -- returns affected, last insert id
+      return r, con:getlastautoid()
+    else -- return rows
+      local rows = {}
+      local row = {}
+      while r:fetch(row, "a") do
+        table.insert(rows, row)
+        row = {}
+      end
+      return msgpack.pack(rows)
+    end
+  end
+
+  connect() -- connect to DB
+
+  -- process queries
   local query, params = ch_in:pop()
   while query do
-    local a, b = nil, nil
-
     params = msgpack.unpack(params)
 
     -- replace params
     for k,v in pairs(params) do
       local ptype = type(v)
       local param = con:escape(tostring(v))
-
       if ptype ~= "number" then
         param = "\'"..param.."\'"
       end
-  
       query = string.gsub(query,"%{"..k.."%}", param)
     end
 
-    -- do query
-    local r,err = con:execute(query)
-    if r == nil then -- error
-      print(err.."[ "..query.." ]")
-    elseif type(r) ~= "userdata" then -- returns affected, last insert id
-      a = r
-      b = con:getlastautoid()
-    else -- return rows
-      a = {}
-      local row = {}
-      while r:fetch(row, "a") do
-        table.insert(a, row)
-        row = {}
-      end
-
-      a = msgpack.pack(a)
+    local a,b = process_query(query)
+    while a == nil and not con:ping() do -- error and invalid connection
+      print("disconnected from database")
+      connect()
+      a,b = process_query(query) -- try again
     end
 
     -- returns
     ch_out:push(a,b)
-
+    -- next
     query, params = ch_in:pop()
   end
 
