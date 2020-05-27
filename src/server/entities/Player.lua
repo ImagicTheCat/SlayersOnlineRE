afe92022643ebaf4f03b67444745bd6507df77b1
imagic <>
2020-05-27 16:01:28 +0000

    Fix map chat issue.
    FossilOrigin-Name: a1ee059b651983e9ae7f5026979278522a4d732d324324ff7a77d41aaf89777b

--- src/server/entities/Player.lua
+++ src/server/entities/Player.lua
@@ -2,14 +2,17 @@ local net = require("protocol")
 local LivingEntity = require("entities.LivingEntity")
 local Mob = require("entities.Mob")
 -- deferred require
-local Map
+local Map, Client
 task(0.01, function()
   Map = require("Map")
+  Client = require("Client")
 end)
 
 local Player = class("Player", LivingEntity)
 
 -- STATICS
+
+local MAP_CHAT_RADIUS = 15 -- cells
 
 -- METHODS
 
@@ -52,7 +55,13 @@ end
 
 function Player:mapChat(msg)
   if self.map then
-    self.map:broadcastPacket(net.MAP_CHAT, {id = self.id, msg = msg})
+    -- send message to all client in chat radius
+    local p = Client.makePacket(net.MAP_CHAT, {id = self.id, msg = msg})
+    for client in pairs(self.map.clients) do
+      local dx = math.abs(self.x-client.x)
+      local dy = math.abs(self.y-client.y)
+      if dx <= MAP_CHAT_RADIUS*16 and dy <= MAP_CHAT_RADIUS*16 then client:send(p) end
+    end
   end
 end
 
