8e3d65c2a3c9c671ce5a06dd174a3d48ce1dfbf4
imagic <>
2020-05-28 15:14:40 +0000

    Fix gameplay issues.
    - attack, etc
    
    FossilOrigin-Name: ccf2bcf42f1f28a552e65a3e4add86c39800feaaabdf8a5e88bba2b1347c764b

--- src/server/entities/Player.lua
+++ src/server/entities/Player.lua
@@ -24,35 +24,6 @@ function Player:__construct()
   self.hurt_sound = "Kill1.wav"
 end
 
--- override
-function Player:onAttack(attacker)
-  if self.ghost or attacker == self then return end
-
-  if class.is(attacker, Mob) then
-    self:damage(attacker:computeAttack(self))
-    return true
-  elseif class.is(attacker, Player) then
-    if self.map and self.map.data.type == Map.Type.PVE then return false end
-
-    -- alignment loss
-    local amount = attacker:computeAttack(self)
-    if amount and self.map and self.map.data.type == Map.Type.PVE_PVP then
-      attacker:setAlignment(attacker.alignment-5)
-    end
-    self:damage(amount)
-    self.last_attacker = attacker
-    return true
-  end
-end
-
--- override
-function Player:serializeNet()
-  local data = LivingEntity.serializeNet(self)
-  data.pseudo = self.pseudo
-  data.guild = self.guild
-  return data
-end
-
 function Player:mapChat(msg)
   if self.map then
     -- send message to all client in chat radius
