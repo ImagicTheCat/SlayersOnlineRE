05c510a4c9c3a358ce5b944794689ccb7472e732
imagic <>
2021-04-04 20:34:25 +0000

    Improve midi conversion script.
    FossilOrigin-Name: b63e1337a11be1067ddf7bb4de1271c95b130e9dd3e246fc7411f75209f8bae3

--- tools/convert_midi.sh
+++ tools/convert_midi.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-# convert all midi files in the current directory to ogg using a soundfont (ex: tools/gm.sf2 Windows GM)
-# parameters: <soundfont path>
+
+# Convert all midi files to ogg using a soundfont (ex: tools/gm.sf2 Windows GM)
+# from the current directory to the target directory (lazily).
+#
+# parameters: <target directory> <soundfont path>
 
 if [ -z "$1" ]
+then
+  echo "missing target directory"
+  exit 1
+fi
+
+if [ -z "$2" ]
 then
   echo "missing soundfont path"
   exit 1
 fi
 
-path=$1
+target=$1
+soundfont=$2
 
-find . -type f -name '*.mid' | sed -e 's/^\(.\+\)\.mid$/\1/g' | xargs -d '\n' -P $(nproc) -n 1 -I % bash -c "fluidsynth -F \"%.wav\" \"$path\" \"%.mid\" 2> /dev/null && ffmpeg -y -i \"%.wav\" -vn -c:a libvorbis -b:a 128k -filter:a loudnorm -ar 44100 \"%.ogg\" && rm \"%.wav\""
+find . -type f -name '*.mid' -printf '%P\n' | parallel "[[ ! -f $target/{/.}.ogg ]] && \
+  echo Convert {}... && \
+  fluidsynth -F $target/{/.}.wav \"$soundfont\" {/.}.mid 2> /dev/null && \
+  ffmpeg -y -i $target/{/.}.wav -vn -c:a libvorbis -b:a 128k -filter:a loudnorm -ar 44100 $target/{/.}.ogg && \
+  rm $target/{/.}.wav"
+
+# Old code using xargs.
+# find . -type f -name '*.mid' -printf '%P\n' | sed -e 's/^\(.\+\)\.mid$/\1/g' | xargs -d '\n' -P $(nproc) -n 1 -I % bash -c "[[ ! -f '$target/%.ogg' ]] && fluidsynth -F \"$target/%.wav\" \"$soundfont\" \"%.mid\" 2> /dev/null && ffmpeg -y -i \"$target/%.wav\" -vn -c:a libvorbis -b:a 128k -filter:a loudnorm -ar 44100 \"$target/%.ogg\" && rm \"$target/%.wav\""
