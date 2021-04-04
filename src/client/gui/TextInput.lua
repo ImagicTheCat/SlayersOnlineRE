692ed911d37ca75173f710202d083bd9d3a2f479
imagic <>
2021-04-04 18:00:37 +0000

    Add hidden text input (e.g. passwords).
    FossilOrigin-Name: 561dc87c509c38dbd452f3f75cc4db19fb2c4bdfc37a2f425a3737ee071896d4

--- src/client/gui/TextInput.lua
+++ src/client/gui/TextInput.lua
@@ -34,12 +34,21 @@ local function focus_change(self, state)
   end
 end
 
+local function update_display(self)
+  if self.hidden then
+    self.display_text:set(string.rep("•", utf8.len(self.text)))
+  else
+    self.display_text:set(self.text)
+  end
+end
+
 -- METHODS
 
 function TextInput:__construct()
   Widget.__construct(self)
 
   self.text = ""
+  self.hidden = false
   self.display_text = love.graphics.newText(love.graphics.getFont())
   self:listen("gui-change", gui_change)
   self:listen("text-input", self.input)
@@ -53,17 +62,24 @@ function TextInput:__construct()
   end
 end
 
+function TextInput:setHidden(hidden)
+  if self.hidden ~= hidden then
+    self.hidden = hidden
+    update_display(self)
+  end
+end
+
 -- data: text
 function TextInput:input(data)
   self.text = self.text..data
-  self.display_text:set(self.text)
+  update_display(self)
   self:trigger("change")
 end
 
 function TextInput:set(text)
   local changed = (self.text ~= text)
   self.text = text
-  self.display_text:set(self.text)
+  update_display(self)
   if changed then self:trigger("change") end
 end
 
@@ -78,7 +94,7 @@ function TextInput:erase(offset)
   local offset = utf8.offset(self.text, offset)
   if offset then
     self.text = string.sub(self.text, 1, offset-1)
-    self.display_text:set(self.text)
+    update_display(self)
     self:trigger("change")
   end
 end
