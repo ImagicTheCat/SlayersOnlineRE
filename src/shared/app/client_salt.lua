e93d477d6e80a71f15b16cfe88cfc76af8093910
imagic <>
2021-10-22 13:45:20 +0000

    Improve client version generation and doc.
    FossilOrigin-Name: 30b80a004769cfc10326920793ac9ae49209fa8ff109fca49b760409304df5ff

--- src/shared/app/client_salt.lua
+++ src/shared/app/client_salt.lua
@@ -1,1 +1,2 @@
+-- The salt is used for password hashing. If modified, all passwords will be invalidated.
 return "cnXJg3yJvqJpxduSF0++LooEE7tQ1d27sEFzEumd7vg7AiZcOiglfnbl4IGATuIp23UgXRvQJmEeh+RbdXb9Yg=="
--- src/shared/app/client_version.lua
+++ src/shared/app/client_version.lua
@@ -1,1 +1,5 @@
-return "db6d9dd78ce2d63e8630d2342fe44bbe0fd809caa38a32318145845304f7293c"
+-- The version must be modified when the client must be updated. Not every
+-- server changes lead to a client update.
+-- For compatibility with the client_release.sh script, keep the version on the
+-- same line as the return keyword.
+return "3b064dd2c6f137cb5c3ba17a57f6d8efe3c13307"
