dde049adb62a23390a6b988a79c6648a4ee7e484
imagic <>
2023-04-24 20:22:00 +0000

    Re-license the project under the MPL-2.0.
    FossilOrigin-Name: 1f3225973935d3ca2932c1a20f44150a6fa5e99d4cd135fe5f332a4218bd7e47

--- src/shared/app/utils.lua
+++ src/shared/app/utils.lua
@@ -1,5 +1,8 @@
--- https://github.com/ImagicTheCat/SlayersOnlineRE
--- MIT license (see LICENSE, src/server/main.lua or src/client/main.lua)
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- Copyright (c) 2019 ImagicTheCat
 
 local utils = {}
 
