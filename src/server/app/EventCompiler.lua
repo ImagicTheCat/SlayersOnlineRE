d44b38cfe60977dea0d66091aebbf82b53dae3bd
imagic <>
2023-05-04 19:04:14 +0000

    Improve event compiler documentation.
    FossilOrigin-Name: 9ee1d9fcc5155e64524719607bccf2d9b50d271bb1d7ca9104c785aedaa5c28a

--- src/server/app/EventCompiler.lua
+++ src/server/app/EventCompiler.lua
@@ -5,16 +5,16 @@
 -- Copyright (c) 2019 ImagicTheCat
 
 -- Compile event conditions/commands to Lua functions.
--- Recursive descent parser with prediction (not LL(k)) and a lexer pass.
---
+
 --[[
-EBNF-like:
-condition = condition_flag | inventory cmp_op expr | expr cmp_op expr_empty
+EBNF-like grammar (not exact, for overview):
+
+condition = condition_flag | inventory cmp_op expr | expr cmp_op expr
 inventory = '%' 'Inventaire' '%'
 condition_flag = 'Appuie sur bouton' | 'Automatique' | 'Auto une seul fois' |
   'En contact' | 'Attaque'
 command = call_condition | call | assignment
-call = text [args | quoted_args]
+call = keyword [args | quoted_args]
 call_condition = 'Condition' '(' ''' condition ''' ')'
 assignment = lvar '=' (concat | expr)
 lvar = var | bool_var | server_var | special_var | event_var
@@ -23,22 +23,19 @@ quoted_args = '(' ''' expr {''' ',' ''' expr} ''' ')'
 cmp_op = '=' | '<=' | '>=' | '<' | '>' | '!='
 var = 'Variable' '[' var_index ']'
 bool_var = 'Bool' '[' var_index ']'
-var_index = range | expr
+var_index = uint ['..' uint]
 server_var = 'Serveur' '[' expr ']'
-special_var = '%' text '%'
-event_var = '%' identifier '.' text '%'
-identifier = identifier_item {identifier_item}
-identifier_item = '-' | whitespace | text
-function_var = '%' text args '%'
+special_var = '%' keyword '%'
+event_var = '%' text '.' keyword '%'
+function_var = '%' keyword args '%'
 input_string = 'InputString' quoted_args
 concat = 'Concat' quoted_args
-range = text '..' text
-expr = expr_item {expr_item}
-expr_empty = {expr_item}
-expr_item = lvar | function_var | input_string | token
-
-Notes:
-- "expr" uses prediction to stop token consumption
+expr = expr_calc | expr_string
+factor = number | expr_construct | '(' expr_calc ')'
+term = factor {('*' | '/') factor}
+expr_calc = term {('+' | '-') term}
+expr_string = {expr_construct | text}
+expr_construct = lvar | function_var | input_string token
 ]]
 
 local lpeg = require "lpeg"
@@ -50,6 +47,8 @@ local M = {}
 
 local P, S, R, V = lpeg.P, lpeg.S, lpeg.R, lpeg.V
 local C, Cc, Ct = lpeg.C, lpeg.Cc, lpeg.Ct
+
+--- Error handling system
 
 local farthest_tokens = {}
 local farthest = 0
@@ -64,6 +63,8 @@ end
 
 local function T(token) return P(token) + lpeg.Cmt(Cc(token), report_token) * P(false) end
 
+--- Grammar
+
 local keyword = C( R("az", "AZ", "09")^1 )
 local event_var = Ct( Cc"event_var" * T"%" * C( (1 - P".")^1 ) * T"." * keyword * T"%" )
 local special_var = Ct( Cc"special_var" * T"%" * keyword * T"%" )
@@ -72,13 +73,15 @@ local uint = C( R"09"^1 )
 local index_range = uint * (T".." * uint)^-1
 local bool_var = Ct( Cc"bool_var" * T"Bool[" * index_range * T"]" )
 local var = Ct( Cc"var" * T"Variable[" * index_range * T"]" )
-
 local lvar = var + bool_var + special_var + event_var + V"server_var"
 local expr_construct = lvar + V"function_var" + V"input_string"
+
+-- expr_calc
 local factor = number + expr_construct + T"(" * V"expr_calc" * T")"
 local term = Ct( Cc"expr_calc" * factor * (C(S"*/") * factor)^0 )
 local expr_calc = Ct( Cc"expr_calc" * term * (C(S"+-") * term)^0 )
 
+-- expr pattern generator
 local function expr(end_pattern)
   end_pattern = P(end_pattern)
   local expr_string_item = expr_construct + C( (-expr_construct * -end_pattern * 1)^1 )
@@ -86,19 +89,21 @@ local function expr(end_pattern)
   return (expr_calc * #end_pattern) + expr_string
 end
 
-local server_var = Ct( Cc"server_var" * T"Serveur[" * expr("]") * T"]" )
-
+-- (quoted) args pattern generator
+-- eol: truthy to match until the end of line
 local function gen_args(s_begin, s_sep, s_end, eol)
   local e = expr(P(s_sep) + P(s_end) * (eol and P(-1) or P(true)))
   return T(s_begin) * e * (T(s_sep) * e)^0 * T(s_end)
 end
 
+-- args
 local quoted_args = gen_args("('", "','", "')")
 local quoted_args_eol = gen_args("('", "','", "')", true)
 local args = gen_args("(", ",", ")")
 local args_eol = gen_args("(", ",", ")", true)
 
 local expr_eol = expr(-1)
+local server_var = Ct( Cc"server_var" * T"Serveur[" * expr("]") * T"]" )
 local input_string = Ct( Cc"input_string" * T"InputString" * quoted_args )
 local function_var = Ct( Cc"function_var" * T"%" * keyword * args * T"%" )
 local concat = Ct( Cc"concat" * T"Concat" * quoted_args_eol )
@@ -106,18 +111,23 @@ local assignment = Ct( Cc"assignment" * lvar * T"=" * (concat + expr_eol) )
 local call = Ct( Cc"call" * keyword * (quoted_args_eol + args_eol)^-1 )
 local condition_flag = C( T"Appuie sur bouton" + T"Automatique" + T"Auto une seul fois" + T"En contact" + T"Attaque" )
 local cmp_op = C( T"=" + T"<=" + T">=" + T"<" + T">" + T"!=" )
+
+-- condition patterns
 local condition; do
   local expr_end = expr(P"')" + -1)
   local condition_inv = Ct( Cc"condition_inv" * T"%Inventaire%" * cmp_op * expr_end )
   condition = condition_flag + condition_inv + Ct( Cc"condition_expr" * expr(cmp_op) * cmp_op * expr_end )
 end
+
 local call_condition = Ct( Cc"call_condition" * T"Condition('" * condition * T"')" )
 local command = assignment + call_condition + call
-
 local comment = P"//" * P(1)^0
+
+--- Final line/instruction patterns
 
 local l_command = P{
   command + comment,
+  -- refs
   expr_calc = expr_calc,
   server_var = server_var,
   input_string = input_string,
@@ -126,6 +136,7 @@ local l_command = P{
 
 local l_condition = P{
   condition + comment,
+  -- refs
   expr_calc = expr_calc,
   server_var = server_var,
   input_string = input_string,
@@ -136,8 +147,12 @@ local l_condition = P{
 
 local function escape(str) return string.format("%q", str) end
 
+-- Functions to browse the AST (abstract syntax tree) and generate Lua code by
+-- returning strings. Uses a state to generate code and to produce metadata.
+
 local gen = {}
 
+-- Dispatch AST element to its specific handler.
 local function dispatch(state, ast) return gen[ast[1]](state, ast) end
 
 function gen.command(state, ast) return dispatch(state, ast) end
@@ -151,7 +166,7 @@ local CONDITION_FLAGS = {
 }
 
 function gen.condition(state, ast)
-  if type(ast) == "string" then
+  if type(ast) == "string" then -- build flags metadata
     state.flags[CONDITION_FLAGS[ast]] = true
     return ""
   else
@@ -180,10 +195,10 @@ function gen.assignment(state, ast)
   elseif lhs[1] == "server_var" then
     prefix = lhs[1].."("..dispatch(state, lhs[2])
   end
-  if rhs[1] == "concat" then
+  if rhs[1] == "concat" then -- rhs: concat
     assert(#rhs == 2, "wrong number of concat arguments")
     return prefix..", "..prefix..").."..dispatch(state, rhs[2])..")"
-  else
+  else -- rhs: expr
     return prefix..", "..dispatch(state, rhs)..")"
   end
 end
@@ -204,7 +219,7 @@ function gen.condition_expr(state, ast)
   -- conversion to Lua operators
   if op == "!=" then op = "~=" end
   if op == "=" then op = "==" end
-  -- gen
+  -- generate
   if op == "==" or op == "~=" then -- string comparison
     return "S("..dispatch(state, lhs)..") "..op.." S("..dispatch(state, rhs)..")"
   else
@@ -225,10 +240,11 @@ end
 
 function gen.call(state, ast)
   local id = ast[2]
+  -- build call code
   local args = { escape(id) }
   for i=3, #ast do table.insert(args, dispatch(state, ast[i])) end
   local call_code = "func("..table.concat(args, ", ")..")"
-  -- query control flow
+  -- implement query control flow
   if id == "InputQuery" then
     return "state.qresult = "..call_code
   elseif id == "OnResultQuery" then
@@ -255,6 +271,7 @@ function gen.expr_calc(state, ast)
       table.insert(args, dispatch(state, arg))
     end
   end
+  -- no parenthesis for singleton
   return #args == 1 and table.concat(args) or "("..table.concat(args)..")"
 end
 
@@ -309,22 +326,26 @@ function gen.input_string(state, ast)
   return "func("..table.concat(args, ", ")..")"
 end
 
+-- Compilation of instructions and pages
+
 local function compileInstruction(itype, state, instruction)
   local parser = itype == "condition" and l_condition or l_command
+  -- error handling
   farthest = 0
   farthest_tokens = {}
+  -- parse
   local ast = parser:match(instruction)
-  if not ast then
+  if not ast then -- error
     local expected_tokens = {}
     for k in pairs(farthest_tokens) do table.insert(expected_tokens, '"'..k..'"') end
     error("unexpected input, expected "..table.concat(expected_tokens, ", ").."\n"..
       instruction.."\n"..string.rep(" ", farthest - 1).."^")
   end
-  if type(ast) == "number" then return "" end
+  if type(ast) == "number" then return "" end -- void instruction, comment
   return gen[itype](state, ast)
 end
 
--- Compile condition block.
+-- Compile condition page/block.
 -- return (code, flags) or (nil, err)
 function M.compileConditions(instructions)
   local state = {flags = {}}
@@ -345,7 +366,7 @@ function M.compileConditions(instructions)
   return (empty and "return true " or "return ")..table.concat(lines, "\n"), state.flags
 end
 
--- Compile command block.
+-- Compile command page/block.
 -- return code or (nil, err)
 function M.compileCommands(instructions)
   local state = {query_count = 0, condition_count = 0}
@@ -362,6 +383,7 @@ function M.compileCommands(instructions)
 end
 
 -- Validate instructions.
+-- Seek for errors without stopping at the first of a page.
 local function validate(itype, instructions)
   local errors = {}
   local state = {query_count = 0, condition_count = 0, flags = {}}
